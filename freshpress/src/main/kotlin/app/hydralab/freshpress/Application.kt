a29f5840f53574c7ba3edf58e676d6aaf23a6637
Marcus Dashe <marcussdashe.developer@gmail.com>
2023-03-25 14:26:47 -0700

    Let me commit to implementing my Ideas

---
+++ freshpress/src/main/kotlin/app/hydralab/freshpress/Application.kt
@@ -0,0 +1,12 @@
+package app.hydralab.freshpress
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.runApplication
+
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ freshpress_fem/app/src/main/java/app/hydralab/freshpress/MainActivity.kt
@@ -0,0 +1,43 @@
+package app.hydralab.freshpress
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import app.hydralab.freshpress.ui.theme.FreshPressTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FreshPressTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FreshPressTheme {
+        Greeting("Android")
+    }
+}
